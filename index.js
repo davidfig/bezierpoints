a33e39273ba13b261e2e3bf68a7e8651ce30f7d0
David Figatner <david@yopeyopey.com>
2017-01-04 14:36:11 +0800

    test better handling of 1 and 2 points

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const touches = require('touches');
 const renderer = new Renderer({autoresize: true, resolution: window.devicePixelRatio});
 renderer.canvas.style.pointerEvents = 'none';
 
-const count = 5;
+const count = 5; // test using count = 1 and 2
 const circles = [];
 const radius = 20, radiusSquared = radius * radius;
 const w = window.innerWidth / (count + 1), h = window.innerHeight;
@@ -17,6 +17,23 @@ for (let i = 0; i < count; i++)
 }
 const g = renderer.add(new PIXI.Graphics());
 draw();
+
+function draw()
+{
+    g.clear()
+        .lineStyle(10, 0, 0.5);
+    bezierPoints(g, circles);
+    g.lineStyle(0);
+    for (let circle of circles)
+    {
+        g.beginFill(0xff0000, 0.5)
+            .drawCircle(circle.x, circle.y, radius)
+            .endFill();
+    }
+    renderer.render();
+}
+
+/****** INPUT CODE *******/
 
 touches(window, {target: renderer.canvas, filtered: true})
     .on('start', down)
@@ -52,19 +69,17 @@ function up()
     _down = false;
 }
 
-function draw()
+function button(text, right, bottom)
 {
-    g.clear()
-        .lineStyle(10, 0, 0.5);
-    bezierPoints(g, circles);
-    g.lineStyle(0);
-    for (let circle of circles)
-    {
-        g.beginFill(0xff0000, 0.5)
-            .drawCircle(circle.x, circle.y, radius)
-            .endFill();
-    }
-    renderer.render();
+    const button = document.body.appendChild(document.createElement('div'));
+    button.style.position = 'fixed';
+    button.innerHTML = text;
+    button.style.right = right;
+    button.style.bottom = bottom;
+    button.style.background = 'rgba(0,0,0,0.5)';
+    button.style.color = 'white';
+    button.style.padding = '1em';
+    return button;
 }
 
 window.onload = require('./highlight-code.js');
